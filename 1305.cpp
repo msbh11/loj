7f14e79408f290904e9c2764f15ed532b878885c
msbh11 <misbaahm4@gmail.com>
2018-01-06 00:04:57 +0600

    Update 1305.cpp

--- 1305.cpp
+++ 1305.cpp
@@ -10,6 +10,8 @@ int main() {
 	
 	d1 = a1 + c1 - b1;
 	d2 = a2 + c2 - b2 ;
+	     //area1 = abs(a1*(b2-c2)+ b1*(c2-a2)+ c1*(a2-b2) );
+	    
     area1 = abs((a1*b2 + b1*c2 + c1*d2 +d1*a2 )-( a2*b1 + b2*c1 + c2*d1 + d2*a1 ))*.5;
     
     printf("Case %d: %d %d %d\n", i,d1, d2, area1 );    
