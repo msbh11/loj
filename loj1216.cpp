2296341a5d06a6ef86966e88d64d62e4444bbdcd
msbh11 <misbaahm4@gmail.com>
2018-01-05 22:29:08 +0600

    Add files via upload

---
+++ loj1216.cpp
@@ -0,0 +1,20 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,i;
+    double vol,r1,r2,p,h,r,R;
+
+    scanf("%d",&t);
+
+    for(i=1;i<=t;i++)
+    {
+        scanf("%lf%lf%lf%lf",&r1,&r2,&h,&p);
+        r = (2*r1 + 2*r2)*h + 2*r1*p -2*r1*h -2*r2*p ;
+        R = r/(2*h);
+        vol = 2*acos(0.0)*p*(r2*r2+R*R+R*r2)/3;
+        printf("Case %d: %lf\n",i,vol);
+    }
+    return 0;
+}
