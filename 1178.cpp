97dc8221b62c56d56f2014116e7a0b8477efe926
msbh11 <misbaahm4@gmail.com>
2018-01-07 02:44:27 +0600

    Create 1178.cpp

---
+++ 1178.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    double  s,a,b,c,d,e, area1,h,f,g, area2 ;
+    int t;
+    cin>>t ;
+   for(int i=1; i<=t; i++){
+	cin >> a>>b>>c>>d;
+	
+	e = (a-c) ;
+	f = ( b*b - d*d + e*e );
+	g = ( f) /(e*2) ;
+    h = sqrt(b*b - g*g) ;
+    area1 = (a+c) * h  * .5 ;
+    
+    printf("Case %d: %lf\n", i,area1 );    
+    }	
+	return 0;
+}
+
+
+
