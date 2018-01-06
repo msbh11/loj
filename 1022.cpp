2a19cd45cc8dcf961c385a5ad88a406b8ed0b3de
msbh11 <misbaahm4@gmail.com>
2018-01-07 03:00:19 +0600

    Create 1022.cpp

---
+++ 1022.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    double  s,a,b,c,d,e, area1,h,f,g, area2 ;
+    int t;
+    cin>>t ;
+    for(int i=1; i<=t; i++){
+	cin >> a;
+	
+	area1 = (2*a)*(2*a) - (2*acos(0.0) * (a*a) ) ;
+    
+    printf("Case %d: %.2lf\n", i,area1 );    
+    }	
+	return 0;
+}
